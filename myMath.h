a04874591742dfa8da03eced6482997998355f87
Abodi-Massarweh <57840504+Abodi-Massarweh@users.noreply.github.com>
2019-11-16 19:28:53 +0200

    Delete myMath.h

--- myMath.h
+++
@@ -1,8 +0,0 @@
-//function declarations
-float add(float , float );
-float sub(float , float );
-double mul(double , double );
-double div(double ,double );
-double Pow(double ,int );
-double Exp(int );
-void program();
